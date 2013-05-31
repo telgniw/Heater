d2c95974ea490ccf75dc216f4b420e6c445c451b
Yi Huang <telgniw@me.com>
2013-05-31 23:21:02 +0800

    Add database interface.

---
+++ db.py
@@ -0,0 +1,21 @@
+#!/usr/env/bin python
+import sqlite3
+
+class Database:
+    def __init__(self, path):
+        self.path = path
+        self._execute_('''
+            CREATE TABLE IF NOT EXISTS Data (
+                id          INTEGER PRIMARY KEY,
+                uv          TINYINT,
+                hum         TINYINT,
+                temp        FLOAT,
+                timestamp   TIMESTAMP DEFAULT CURRENT_TIME
+            )
+        ''')
+
+    def _execute_(self, sql, params=None):
+        conn = sqlite3.connect(self.path)
+        c = conn.cursor()
+        c.execute(sql, params) if params else c.execute(sql)
+        conn.commit()
