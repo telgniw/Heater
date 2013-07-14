0084248b5bfa975ae5af2e824988e2e31d709b2b
Yi Huang <telgniw@me.com>
2013-07-14 06:44:08 +0000

    Commented web.js.

--- web.js
+++ web.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var sqlite3 = require('sqlite3');
 
-var base_url = '/heater/api';
+////////////////////////////////////////
+// Default settings.
+////////////////////////////////////////
+var base_url = '/heater';
+var api_url = base_url + '/api';
+
 var day_format = '%Y-%m-%d';
-var hour_format = '%H';
 
-var db = new sqlite3.Database('data/uv.sqlite3');
-
+////////////////////////////////////////
+// App initialization.
+////////////////////////////////////////
 var app = express();
+var db = new sqlite3.Database('data/uv.sqlite3');
 
 app.use(function(req, res, next) {
     res.charset = 'utf-8';
     next();
 });
 
-app.get(base_url, function(req, res) {
+////////////////////////////////////////
+// API: get location list
+////////////////////////////////////////
+app.get(api_url, function(req, res) {
     var sql = 'SELECT place AS location, ' + 
               '       MIN(strftime("' + day_format + '", timestamp)) AS first_date, ' +
               '       MAX(strftime("' + day_format + '", timestamp)) AS last_date ' +
@@ -27,7 +36,10 @@ app.get(base_url, function(req, res) {
     });
 });
 
-app.get(base_url + '/:location', function(req, res) {
+////////////////////////////////////////
+// API: get timespan for location
+////////////////////////////////////////
+app.get(api_url + '/:location', function(req, res) {
     var sql = 'SELECT place AS location, ' + 
               '       MIN(strftime("' + day_format + '", timestamp)) AS first_date, ' +
               '       MAX(strftime("' + day_format + '", timestamp)) AS last_date ' +
@@ -40,9 +52,13 @@ app.get(base_url + '/:location', function(req, res) {
     });
 });
 
-app.get(base_url + '/:location/:date', function(req, res) {
+////////////////////////////////////////
+// API: get uv list for each hour for
+//      location and date
+////////////////////////////////////////
+app.get(api_url + '/:location/:date', function(req, res) {
     console.log(req.params);
-    var sql = 'SELECT uv, strftime("' + hour_format + '", timestamp) AS hour ' +
+    var sql = 'SELECT uv, strftime("%H", timestamp) AS hour ' +
               'FROM Data ' +
               'WHERE place = $location AND ' +
               '      strftime("' + day_format + '", timestamp) = $date '
