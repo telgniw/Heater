123ca89f6189e258ae282b8899014169b1f04e15
Yi Huang <telgniw@me.com>
2013-07-23 08:04:29 +0000

    Finish line chart.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,55 @@
-var drawLineChart = function(data) {
+var drawLineChart = function(data, field) {
+    var width = 800, height = 320;
+    var margin = { top: 10, right: 30, bottom: 30, left: 30 };
+    var x = d3.time.scale()
+        .range([0, width]);
+    var y = d3.scale.linear()
+        .range([height, 0]);
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient('bottom');
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left');
+    var line = d3.svg.line()
+        .x(function(d) { return x(d.time); })
+        .y(function(d) { return y(d[field]); });
+
+    var svg = d3.select('#container').select('svg');
+    if(svg.empty()) {
+        svg = d3.select('#container').append('svg')
+            .attr('width', margin.left + width + margin.right)
+            .attr('height', margin.top + height + margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+        svg.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0, ' + height + ')');
+        svg.append('g')
+            .attr('class', 'y axis');
+
+        svg.append('path')
+            .attr('class', 'line');
+    }
+
+    x.domain(d3.extent(data, function(d) { return d.time; }));
+    svg.select('g.x.axis')
+        .transition()
+        .duration(1000)
+        .call(xAxis);
+
+    y.domain(d3.extent(data, function(d) { return d[field]; }));
+    svg.select('g.y.axis')
+        .transition()
+        .duration(1000)
+        .call(yAxis);
+
+    svg.select('path.line')
+        .datum(data)
+        .transition()
+        .duration(1000)
+        .attr('d', line);
 };
 
 $(function() {
@@ -22,7 +73,13 @@ $(function() {
         var date = $('#nav-datepicker').children('input').val();
 
         $.getJSON('api/' + location + '/' + date, function(data) {
-            drawLineChart(data);
+            var parseDate = d3.time.format('%Y-%m-%d %H').parse;
+            data.map(function(d) {
+                d.time = parseDate(date + ' ' + d.hour);
+                return d;
+            });
+
+            drawLineChart(data, 'uv');
         });
     };
 
