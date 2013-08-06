75fbd6fe4f4329c3e945277d3f9ede30a90bd2c2
Yi Huang <telgniw@me.com>
2013-08-06 06:39:32 +0000

    I forgot what I've changed.

--- public/js/vis/line.js
+++ public/js/vis/line.js
@@ -1,3 +1,15 @@
 $(function() {
+    var onLocationSelected = function($place, $chart) {
+    };
+
     $('#navbar-vis').addClass('active');
+
+    $('.row-fluid > .span6').each(function() {
+        var $chart = $('.chart', $(this));
+        $('.menu-item > a', $(this)).click(function() {
+            onLocationSelected($(this), $chart);
+        });
+    });
+
+    $(window).resize(updateVisualization);
 });
